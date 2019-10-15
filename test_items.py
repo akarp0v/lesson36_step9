2fd2768b139068bc89ae98c69d8d16201ee5ce8a
Artem Karpov <komhleb@yandex.ru>
2019-10-15 23:02:02 +0300

    add to cart btn avaiability and switch language features released

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import time
+import pytest
+from selenium import webdriver
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_button_add_to_basket_is_exist(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), \
+        'Button "Add to basket" was NOT found'
