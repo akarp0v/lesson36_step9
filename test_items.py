014740c181247ab8ea01bb892a800648708241f0
Artem Karpov <komhleb@yandex.ru>
2019-10-15 23:07:33 +0300

    code optimization

--- test_items.py
+++ test_items.py
@@ -8,6 +8,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_is_exist(browser):
     browser.get(link)
-    time.sleep(30)
+    # time.sleep(30)
     assert browser.find_element_by_css_selector(".btn-add-to-basket"), \
         'Button "Add to basket" was NOT found'
