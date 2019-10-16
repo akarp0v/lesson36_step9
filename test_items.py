84db722652cbd76210494f543791cb8ec2df5a3e
Artem Karpov <komhleb@yandex.ru>
2019-10-16 10:08:40 +0300

    fix naming

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_button_add_to_basket_is_exist(browser):
+def test_button_add_to_basket_is_displayed(browser):
     browser.get(link)
     # time.sleep(30)
     assert browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed(), \
